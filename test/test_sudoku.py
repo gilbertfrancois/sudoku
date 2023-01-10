38722c4e05bbb7c14d7b80ae5fdcff933008812c
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2023-01-11 00:11:17 +0100

    Test non-unique solutions

--- test/test_sudoku.py
+++ test/test_sudoku.py
@@ -6,9 +6,9 @@ from sudoku import Sudoku
 def solve_puzzle_from_file(filename: str) -> tuple[bool, bool]:
     sudoku = Sudoku()
     sudoku.load(filename)
-    status = sudoku.solve()
+    num_solutions = sudoku.solve()
     is_solved = sudoku.is_solved()
-    return status, is_solved
+    return num_solutions, is_solved
 
 
 class TestSudoku(TestCase):
@@ -44,21 +44,63 @@ class TestSudoku(TestCase):
         status = sudoku._possible(5, 1, 8)
         self.assertFalse(status)
 
-    def test_examples(self):
+    def test_easy(self):
         filenames = [
             "../examples/easy_001.txt",
             "../examples/easy_002.txt",
             "../examples/easy_003.txt",
+        ]
+        for filename in filenames:
+            num_solutions, is_solved = solve_puzzle_from_file(filename)
+            self.assertEqual(num_solutions, 1)
+            self.assertTrue(is_solved)
+
+    def test_medium(self):
+        filenames = [
             "../examples/medium_001.txt",
             "../examples/medium_002.txt",
+        ]
+        for filename in filenames:
+            num_solutions, is_solved = solve_puzzle_from_file(filename)
+            self.assertEqual(num_solutions, 1)
+            self.assertTrue(is_solved)
+
+    def test_hard(self):
+        filenames = [
             "../examples/hard_001.txt",
             "../examples/hard_002.txt",
+        ]
+        for filename in filenames:
+            num_solutions, is_solved = solve_puzzle_from_file(filename)
+            self.assertEqual(num_solutions, 1)
+            self.assertTrue(is_solved)
+
+    def test_expert(self):
+        filenames = [
             "../examples/expert_001.txt",
             "../examples/expert_002.txt",
+        ]
+        for filename in filenames:
+            num_solutions, is_solved = solve_puzzle_from_file(filename)
+            self.assertEqual(num_solutions, 1)
+            self.assertTrue(is_solved)
+
+    def test_evil(self):
+        filenames = [
             "../examples/evil_001.txt",
             "../examples/evil_002.txt",
         ]
         for filename in filenames:
-            status, is_solved = solve_puzzle_from_file(filename)
-            self.assertTrue(status)
+            num_solutions, is_solved = solve_puzzle_from_file(filename)
+            self.assertEqual(num_solutions, 1)
+            self.assertTrue(is_solved)
+
+    def test_non_unique(self):
+        filenames = [
+            "../examples/non_unique_001.txt",
+            "../examples/non_unique_002.txt",
+        ]
+        for filename in filenames:
+            num_solutions, is_solved = solve_puzzle_from_file(filename)
+            self.assertGreater(num_solutions, 1)
             self.assertTrue(is_solved)
